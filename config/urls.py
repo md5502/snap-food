0712ff81a2279cce41ee626a35830917aedd393c
md5502 <m.baniasadi.d@gmail.com>
2024-12-09 10:44:28 +0330

    add ruff and format the code

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,4 @@
-"""
-URL configuration for config project.
+"""URL configuration for config project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -18,5 +17,5 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 ]
