8ac1970d9de5fc4173e80531bdb677af4c0b8d49
md5502 <m.baniasadi.d@gmail.com>
2024-12-16 22:20:04 +0330

    create comment views and template tags

--- comment/admin.py
+++ comment/admin.py
@@ -1,2 +1,6 @@
+from django.contrib import admin
 
-# Register your models here.
+from .models import Comment, Reaction
+
+admin.site.register(Comment)
+admin.site.register(Reaction)
--- comment/models.py
+++ comment/models.py
@@ -1,2 +1,56 @@
 
-# Create your models here.
+from django.conf import settings
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+
+
+class Comment(models.Model):
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.UUIDField()
+    content_object = GenericForeignKey("content_type", "object_id")
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="comments",
+    )
+    like_count = models.IntegerField(default=0)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    parent = models.ForeignKey(
+        "self",
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+        related_name="replies",
+    )
+
+    def __str__(self):
+        return f"{self.user.email}: {self.content[:20]}..."
+
+    def is_reply(self):
+        return bool(self.parent)
+
+
+class Reaction(models.Model):
+    REACTION_CHOICES = (
+        ("like", "Like"),
+        ("dislike", "Dislike"),
+    )
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="reactions",
+    )
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name="reactions")
+    reaction_type = models.CharField(max_length=10, choices=REACTION_CHOICES)
+
+    class Meta:
+        unique_together = ("user", "comment", "reaction_type")
+
+    def __str__(self):
+        return f"{self.user.email} - {self.reaction_type} on {self.comment.id}"
